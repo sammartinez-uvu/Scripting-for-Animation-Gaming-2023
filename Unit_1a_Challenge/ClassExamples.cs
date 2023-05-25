68bd2ae4229c229c59edf5fd5f283c8d95345207
Samuel Martinez <133166889+sammartinez-uvu@users.noreply.github.com>
2023-05-25 11:39:04 -0600

    Created new variables and functions

--- Unit_1a_Challenge/ClassExamples.cs
+++ Unit_1a_Challenge/ClassExamples.cs
@@ -10,6 +10,8 @@ public class ClassWithVariables
 {
     public int number = 2;
     public string userName = "Bob";
+    public bool hasMoney = true;
+    public double bankTotal = 300.52;
 }
 
 public class ClassWithFunctions
@@ -17,5 +19,13 @@ public class ClassWithFunctions
     public void RunFunction()
     {
         Console.WriteLine("Running Function");
+    }
+    public void SpendMoney()
+    {
+        Console.WriteLine("How much money would you like to spend?")
+    }
+    public void SaveMoney()
+    {
+        Console.WritingLine("Good job at being responsible!")
     }
 }   
