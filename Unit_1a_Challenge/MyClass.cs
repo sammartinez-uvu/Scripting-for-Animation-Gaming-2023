78d5c39a058d275a507c8e7deb3252135a726546
Samuel Martinez <133166889+sammartinez-uvu@users.noreply.github.com>
2023-05-25 16:24:51 -0600

    My Classes code changed to Player Creation code

--- Unit_1a_Challenge/MyClass.cs
+++ Unit_1a_Challenge/MyClass.cs
@@ -1,9 +1,34 @@
-//Use this file to store your work to demonstrate your understanding of operators
-//Make sure your work is different from the instructors examples or your peers
+//This code is designed to have player 1 and 2 change their character's details (inputs): name, age, and class
+//Code outputs a console line of text using those variables to describe the character
 
-//Based on the HelloWorld File, turn this into a class.
+using System;
+					
+public class PlayerCreation
+{
 
-//Write a console program with a program class, and Main Function
-//Add another class with any name, add variables 
-//Using the new keyword create several objects
-//Console writeline the Objects variables
+	public void Main()
+	{
+		playerOne = new PlayerCreation();
+		playerTwo = new PlayerCreation();
+		
+        playerOne.name = "name_here";
+        playerOne.age = "age_here";
+        playerOne.characterClass = "Wizard";
+
+        playerTwo.name = "name_here";
+        playerTwo.age = "age_here";
+        playerTwo.characterClass = "Rouge";
+		
+		
+		Console.WriteLine("Hello " + playerOne.name + ", you just had a birthday yes? Wow you're already " + playerOne.age + "?");
+        Console.WriteLine(" Wow and you've already started to train as a " + playerOne.characterClass);
+
+		Console.WriteLine("Hello " + playerTwo.name + ", you just had a birthday yes? Wow you're already " + playerTwo.age + "?");
+        Console.WriteLine(" Wow and you've already started to train as a " + playerTwo.characterClass);
+	}
+	public PlayerCreation playerOne;
+    public PlayerCreation playerTwo;
+	public string name;
+	public string age;
+	public string characterClass;
+}
