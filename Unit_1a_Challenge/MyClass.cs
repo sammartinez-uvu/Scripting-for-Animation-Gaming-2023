0372033e43f57ce6313673e823f2bb950e8fb08c
Samuel Martinez <133166889+sammartinez-uvu@users.noreply.github.com>
2023-05-25 16:26:39 -0600

    Adding comments to clarify the code

--- Unit_1a_Challenge/MyClass.cs
+++ Unit_1a_Challenge/MyClass.cs
@@ -8,21 +8,25 @@ public class PlayerCreation
 
 	public void Main()
 	{
-		playerOne = new PlayerCreation();
+		//creates instances of characters 1&2
+        playerOne = new PlayerCreation();
 		playerTwo = new PlayerCreation();
 		
+        //Initializes variables for player 1
         playerOne.name = "name_here";
         playerOne.age = "age_here";
         playerOne.characterClass = "Wizard";
 
+        //Initializes variables for player 2
         playerTwo.name = "name_here";
         playerTwo.age = "age_here";
         playerTwo.characterClass = "Rouge";
 		
-		
+		//Outputs text for player 1's character
 		Console.WriteLine("Hello " + playerOne.name + ", you just had a birthday yes? Wow you're already " + playerOne.age + "?");
         Console.WriteLine(" Wow and you've already started to train as a " + playerOne.characterClass);
 
+        //Outputs text for player 2's character
 		Console.WriteLine("Hello " + playerTwo.name + ", you just had a birthday yes? Wow you're already " + playerTwo.age + "?");
         Console.WriteLine(" Wow and you've already started to train as a " + playerTwo.characterClass);
 	}
