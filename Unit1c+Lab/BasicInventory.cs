f1976a60eea3d56863eac0a2af9f670c932e378e
Samuel Martinez <133166889+sammartinez-uvu@users.noreply.github.com>
2023-06-06 16:29:33 -0600

    Commenting added

--- Unit1c+Lab/BasicInventory.cs
+++ Unit1c+Lab/BasicInventory.cs
@@ -4,11 +4,13 @@ public class Program
 {
 	public static void Main()
 	{
+        //Displays the Inventory
 		string[] inventory = {"Diamonds", "Gold", "Silver", "Iron"};
 		foreach(var currentInventory in inventory){
 			inventory[0] = "Empty";
 			Console.WriteLine(currentInventory);
 		}
+        //Displays an updated Inventory after changing slot 0 to "Empty"
 		foreach(var currentInventory in inventory){
 			inventory[0] = "Empty";
 			Console.WriteLine(currentInventory);
