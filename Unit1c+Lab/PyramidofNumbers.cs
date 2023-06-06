de75b99231fbf1f4baf0262d6d810fc2478e06c9
Samuel Martinez <133166889+sammartinez-uvu@users.noreply.github.com>
2023-06-06 16:08:47 -0600

    Updating Pyramid of Numbers Challenge

--- Unit1c+Lab/PyramidofNumbers.cs
+++ Unit1c+Lab/PyramidofNumbers.cs
@@ -4,14 +4,14 @@ public class Program
 {
 	public static void Main()
 	{
-		int[] pyramid = {1, 2, 3, 4, 5};
-		for(int i = 0; i < pyramid.Length; i++){
-			Console.WriteLine(pyramid[i]);
+		int rows = 5;
+		for(int i = 0; i < rows ; i++){
+			
 			for(int j = 0; j < i; j++){
-				Console.Write(i);
+				Console.Write(i+1);
 			}
+			Console.WriteLine(i+1);
 			
 		}
 	}
-	
 }
