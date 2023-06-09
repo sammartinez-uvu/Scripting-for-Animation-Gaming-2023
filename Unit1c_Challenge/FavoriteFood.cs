1b86b8a150739ddeb2f6beab4bb74779f9e48e5c
Samuel Martinez <133166889+sammartinez-uvu@users.noreply.github.com>
2023-06-08 21:24:26 -0600

    Updating FavoriteFoods with a Switch Statement

--- Unit1c_Challenge/FavoriteFood.cs
+++ Unit1c_Challenge/FavoriteFood.cs
@@ -1,10 +1,4 @@
-/*
-This code is designed to take the favoriteFood array and display it with "I love " + the array item
-I wanted to make this more complex but I didn't just want to use a bunch of if statements, example if(i == 0){Console.WriteLine("Blah Blah Blah")}
-I tried using a switch statement but I couldn't get it to work with the array without making a huge enum side quest
-
-*/
-
+///This code is designed to display favorite foods.  Added items to the array past 4 will result in "An honorable mention goes to _______"
 
 using System;
 					
@@ -12,12 +6,28 @@ public class Program
 {
 	public static void Main()
 	{
-        //User's Favorite Food.  More food can be added by using a ',' then the string "Chips"
-		string[] favoriteFood = {"Tacos", "Hamburger", "Pizza", "General Tao Chicken"};
+        //User's Favorite Food.  More food can be added by using a: ',' then the string: "Chips"
+		string[] favoriteFood = {"Tacos", "Hamburger", "Pizza", "General Tao Chicken", "Pringles"};
 		//Loops until the end of the array
 		for(int i = 0; i < favoriteFood.Length; i++){
-
-				Console.WriteLine("I love " + favoriteFood[i]);
+			switch(i){
+				case 0:
+					Console.WriteLine("I love " + favoriteFood[i]);
+					break;
+				case 1:
+					Console.WriteLine("A close second is a " + favoriteFood[i]);
+					break;
+				case 2:
+					Console.WriteLine("Although having " + favoriteFood[i] + " is always a good choice");
+					break;
+				case 3:
+					Console.WriteLine("And don't forget " + favoriteFood[i]);
+					break;
+				case > 3:
+					Console.WriteLine("An honorable mention goes to " + favoriteFood[i]);
+					break;
+					
+			}
 		}
 	}
 }
