c67ffb48e2dcf924e20a05a786dd4c2f22faf111
Samuel Martinez <10789309@uvu.edu>
2023-06-14 20:40:14 -0600

    Trying to Figure out what InputAxis Does

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// This script is not ready for use, I am still trying to understand the Input.GetAxis Stuff 
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
+    private float moveSpeed = 5f;
+    private float jumpForce = 5f;
+    private float gravity = 9.81f;
+
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    private bool isJumping;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<CharacterController>()
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
     }
 }
