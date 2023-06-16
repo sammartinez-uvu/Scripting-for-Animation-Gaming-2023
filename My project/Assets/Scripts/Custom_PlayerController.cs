5e1de3067de048223f100ca58c32bfb18cb853af
Samuel Martinez <10789309@uvu.edu>
2023-06-16 16:48:13 -0600

    Fixed Player Controller to be Character Controller
    I wasn't using the Character Controller component for the last one so now I am fixing that.  The old Player Controller is now Custom Player Controller

--- My project/Assets/Scripts/Custom_PlayerController.cs
+++ My project/Assets/Scripts/Custom_PlayerController.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
 
+/// <summary>
+/// This script is not ready for use, I am still trying to understand the Input.GetAxis Stuff 
+/// </summary>
+public class Custom_PlayerController : MonoBehaviour
+{
     private float moveSpeed = 5f;
     private float jumpForce = 5f;
     private float gravity = 9.81f;
-
+    
 
     private CharacterController controller;
     private Vector3 moveDirection;
     private bool isJumping;
-    private bool isGrounded;
-
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -28,23 +29,10 @@ public class PlayerController : MonoBehaviour
         float horizontallInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        if (controller.isGrounded && Input.GetButtonDown("Jump")) { 
-            isJumping = true;
-        }
-        
-        controller.Move(moveDirection * Time.deltaTime);
+        //Moving in x and z directions
+        transform.Translate(moveSpeed * horizontallInput * Time.deltaTime, 0f, moveSpeed * verticalInput * Time.deltaTime);
 
-        if(isJumping == false) {
+        //Jumping
 
-
-            if (isJumping == true)
-            {
-                moveDirection.y = jumpForce;
-            }
-            else {
-                moveDirection.y = gravity * Time.deltaTime;
-
-            }
-        }
     }
 }
