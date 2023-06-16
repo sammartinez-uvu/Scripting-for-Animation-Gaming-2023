26c7e045125265f6a3c5aa3181f95ab26b9c8729
Samuel Martinez <10789309@uvu.edu>
2023-06-16 16:17:27 -0600

    Figuring out basic movement

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
     private float moveSpeed = 5f;
     private float jumpForce = 5f;
     private float gravity = 9.81f;
+    
 
     private CharacterController controller;
     private Vector3 moveDirection;
@@ -19,14 +20,14 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<CharacterController>()
+        GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-
+        float horizontallInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(moveSpeed * horizontallInput * Time.deltaTime, 0f, moveSpeed * verticalInput * Time.deltaTime);
     }
 }
