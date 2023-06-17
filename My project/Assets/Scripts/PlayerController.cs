87cc0cead3816ef5681a6ba56abfe55e1b2d7c2e
Samuel Martinez <10789309@uvu.edu>
2023-06-16 23:32:00 -0600

    Troubleshooting Gravity
    gravity won't update, even though it's in the update???

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Code designed to make a cube move with the Character Controller component. It moves but I can't get gravity to work on it.  Still troubleshooting it
+/// The gravity solves and at the beginning of the game it works, but then it doesn't update almost as if it was in the Start() but it's not
+/// Any suggestions to fix this would be appreciated!
+/// </summary>
+
 public class PlayerController : MonoBehaviour
 {
 
@@ -14,6 +20,14 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
 
+
+    //jumping variables
+    private float gravity = 9.81f;
+    private bool isGrounded = true;
+    private bool isJumping = true;
+    private float jumpForce = 5f;
+ 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,32 +39,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        //Basic movement and Inputs
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-
+        
         moveDirection = new Vector3(horizontalInput * moveSpeed, 0, verticalInput * moveSpeed);
+ 
         controller.Move(moveDirection * Time.deltaTime);
 
-        /*
-        if (controller.isGrounded && Input.GetButtonDown("Jump")) { 
-            isJumping = true;
-        }
-        
-        
-
-        if(isJumping == false) {
+        //Gravity but it doesn't work???
+        moveDirection.y = gravity * Time.deltaTime;
 
 
-            if (isJumping == true)
-            {
-                moveDirection.y = jumpForce;
-            }
-            else {
-                moveDirection.y = gravity * Time.deltaTime;
 
-            }
-        
-        }
-        */
+
+
     }
 }
