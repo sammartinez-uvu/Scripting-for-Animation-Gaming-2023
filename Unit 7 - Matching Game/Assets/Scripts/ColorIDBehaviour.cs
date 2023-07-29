4da98e4da8b14b5e42bb5fc66fa0bc5b05679d22
Samuel Martinez <10789309@uvu.edu>
2023-07-28 22:41:00 -0600

    Instance and 321 Go! Upload
    All scripts and Unity files upload.  Having trouble with randomized color but otherwise everything else went smooth

---
+++ Unit 7 - Matching Game/Assets/Scripts/ColorIDBehaviour.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorIDBehaviour : IDContainerBehaviour
+{
+    public ColorIDDataList colorIDDataListObj;
+
+    private void Awake() {
+
+        idObj = colorIDDataListObj.currentColor;
+    }
+}
---
+++ Unit 7 - Matching Game/Assets/Scripts/Vector3DataList.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Vector3DataList : ScriptableObject
+{
+
+    public List<Vector3Data> vector3DList;
+
+}
